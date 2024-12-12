2de58063061ac0b13efbf1530d572324e5d4fac3
Mohit <apple@apples-MacBook-Pro.local>
2024-12-12 11:45:38 +0530

    Updated useDebounce to TS

--- custom-hooks/useDebounce/src/App.tsx
+++ custom-hooks/useDebounce/src/App.tsx
@@ -1,21 +1,19 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import useDebounce from "./hooks/useDebounce";
 
 export default function App() {
   const [inputText, setInputText] = useState("");
 
-  function handleChange(e) {
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
     setInputText(e.target.value);
   }
 
-  const debouncedValue = useDebounce(inputText, 3000, () => {
-    console.log("Handle logic here");
-  });
+  const debouncedValue = useDebounce(inputText, 1000);
 
   return (
     <div className="container">
       <input type="text" value={inputText} onChange={handleChange} />
-      <p>{debouncedValue}</p>
+      <p>{debouncedValue ?? ''} </p>
     </div>
   );
 }
--- custom-hooks/useDebounce/src/hooks/useDebounce.tsx
+++ custom-hooks/useDebounce/src/hooks/useDebounce.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 
-const useDebounce = (value, delay, cb = () => {}) => {
+const useDebounce = <T,>(
+  value: T,
+  delay: number,
+): T => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
     const handler = setTimeout(() => {
       setDebouncedValue(value);
-      cb();
     }, delay);
 
     return () => clearTimeout(handler);
